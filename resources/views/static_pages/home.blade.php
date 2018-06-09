0483efff4c727daa5107b5f5eee9839a7864e983
livephone <livephone1988@163.com>
2018-06-09 17:30:12 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello World</h1>
+        <p class="lead">
+            你现在所看到的是<a href="http://www.netoper.com">极客学派</a>的示例主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
